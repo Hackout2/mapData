0e6379c735648ce32fe430df764dcddb95f7093a
Karthik Ram <karthik.ram@gmail.com>
2015-02-11 12:56:33 +0000

    Initial commit of mapData package

---
+++ R/utility_functions.R
@@ -0,0 +1,11 @@
+
+#' Geocode a location
+#'
+#' @param location The name of a location
+#' @importFrom ggmap geocode
+#' @export
+md_geocoder <- function(location = NULL) {
+  if(!is.null(location)) {
+    geocode(location)
+  }
+}
