62de798bdddf3271515d3b3cd091a8b59bc0b566
Joanna Lewis <joanna.lewis@ucl.ac.uk>
2015-02-11 15:15:20 +0000

    Added some documentation to prevalence function.

--- R/calculate_prevalence.R
+++ R/calculate_prevalence.R
@@ -1,17 +1,20 @@
 # function to calculate prevalence, by aggregated region
 
-# data is a line list-type data frame (one line per case), including a column with the region in which the case occurred
-# pops is a data frame containing one line per region, with one column for region name and another for population
-# region is a string containing the name of the column specifying region
+#' Calculate prevalence within each region of aggregation
 
-# returns a data frame containing total population, number of cases and prevalence by region
+#' @param data A line list-type data frame (one line per case), including a column with the region in which the case occurred
+#' @param pops A data frame containing one line per region in "data", with first column for region name and second for population
+#' @param region A string containing the name of the column specifying the region for each case
+
+#' @return A data frame containing total population, number of cases and prevalence by region
 
 calculate_prevalence <- function ( data, pops=NULL, region.head="region" ){
 
 	regioncol <- which(names(data) == region.head)
 
 	data[,regioncol] <- factor( sanitize_text(as.character(data[,regioncol])) )
-	
+	 
+	##################################
 	# make up some populations for now. delete this later
 	if(is.null(pops))
 		pops <- data.frame(
@@ -19,6 +22,9 @@ calculate_prevalence <- function ( data, pops=NULL, region.head="region" ){
 					population = as.numeric(table(data$region)) * 
 						(rlnorm( length(unique(data$region))) + 1)
 					)
+	##################################
+					
+	names(pops) <-c("region", "population")
 	
 	prev <- as.data.frame(tapply(data$departement, data$departement, length))	
 	names(prev) <- "cases"
