7cfa64abdb6b4ac4a595d768213bab381dead237
Joanna Lewis <joanna.lewis@ucl.ac.uk>
2015-02-12 17:22:57 +0000

    Split function for density estimation into data manipulation and density estimation.

--- R/estimate_density.R
+++ R/estimate_density.R
@@ -1,26 +1,12 @@
-# function to estimate 2d density of cases or prevalence
+# function to estimate 2d density, from data frame output by density_data
 
-#' Produce a 2-d estimated intensity function (cases per unit area) from geographical location of cases. Weighting by 1/(local population density) gives an estimate of prevalence.
+#' Produce a 2-d estimated intensity function (cases per unit area) from an (x, y, (corrected) counts) data frame. 
 
-#' @param points SpatialPointsDataFrame containing the line list of patients. Should include: 1) a slot @coords with an n x 2 vector giving the x and y coordinates of each case. (More than one case can have the same co-ordinates.) 2) a slot @bbox giving the maximum and minimum x and y co-ordinates. 
-
-#' @param weights If required, the local population density at each (x, y) coordinate.
+#' @param density_data A data frame with columns x, y and (corrected) number of individuals, as output by the density_data function.
 
 #' @return A SpatialGridDataFrame object containing intensity/prevalence estimates (@data$v), at a grid of points (@grid). 
 
-estimate_density <- function(points, weights=NULL){
-	
-	# x co-ordinates
-	xs <- pointspatients@coords[which(!duplicated(paste(pointspatients@coords[,1], pointspatients@coords[,2]))),1]
-
-	# y co-ordinates
-	ys <- pointspatients@coords[which(!duplicated(paste(pointspatients@coords[,1], pointspatients@coords[,2]))),2]
-
-	# number of cases at each point
-	ns <- tapply(
-					pointspatients@data[,1],
-					paste(pointspatients@coords[,1], pointspatients@coords[, 2], sep=","),
-					length)
+estimate_density <- function(density_data){
 	
 	# construct a ppp object for use by the density estimator			
 	my.ppp <- ppp(
@@ -33,12 +19,7 @@ estimate_density <- function(points, weights=NULL){
 
 	# density estimator
 	
-	if(is.null(weights))
-		my.weights <- NULL
-	else
-		my.weights <- weights[which(!duplicated(paste(pointspatients@coords[,1], pointspatients@coords[,2])))]
-
-	my.density <- density(	my.ppp, my.weights)
+	my.density <- density(	my.ppp)
 	
 	# return as a spatial grid data frame
 	return(as.SpatialGridDataFrame.im( my.density ) )
