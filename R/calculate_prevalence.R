18a99a0cf8ffdfdae6d659f74a8a4c849283aedb
Joanna Lewis <joanna.lewis@ucl.ac.uk>
2015-02-13 11:43:59 +0000

    Added examples.

--- R/calculate_prevalence.R
+++ R/calculate_prevalence.R
@@ -3,12 +3,30 @@
 #' Calculate prevalence within each region of aggregation
 
 #' @param data A line list-type data frame (one line per case), including a column with the region in which the case occurred
-#' @param pops A data frame containing one line per region in "data", with first column for region name and second for population
-#' @param region A string containing the name of the column specifying the region for each case
+#' @param pops A data frame containing one line per region in \code{data}, with first column for region name and second for population
+#' @param region.head A string containing the name of the column specifying the region for each case
 #' @param conf.level Level of confidence interval required for prevalence estimates.
 #' @param scale Scaling with which to report prevalence (per head, per 100 000, etc.)
 
 #' @return A data frame containing total population, number of cases and prevalence by region
+
+#' @examples
+#' my.data <- data.frame(	county.id = ceiling(3*runif(10)),
+#'                      	age = rlnorm(10),
+#'							sex = factor(floor(2*runif(10)), levels=c(0,1), labels=c("male", "female"))
+#'                      )
+#'
+#' my.populations <- data.frame(county.id = 1:3,
+#'								population = c(10, 50, 100)
+#'								)
+#'
+#' # example without population of each region
+#' calculate_prevalence(my.data, region.head="county.id")
+#'
+#' # example with populations
+#' calculate_prevalence(my.data, region.head="county.id", pops=my.populations)
+#'
+
 
 calculate_prevalence <- function ( data, pops=NULL, conf.level=0.95, region.head="region", scale=1 ){
 
