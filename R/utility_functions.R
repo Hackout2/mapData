8d6e226517f1aea2c5d4a104a1ed8a93412ba94d
joanna-lewis <joanna.lewis@imperial.ac.uk>
2015-09-28 15:12:41 +0100

    calculate_prevalence now no longer reports population 1 where populations not given.

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -4,6 +4,8 @@
 #' @param location The name of a location
 #' @importFrom ggmap geocode
 #' @export
+#' @examples
+#' # For examples, see examples for the geocode function (ggmap package).
 md_geocoder <- function(location = NULL) {
     if (!is.null(location)) {
         geocode(location)
